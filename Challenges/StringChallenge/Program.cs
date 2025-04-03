9d2e47a8cd72fcf07f2212184e1c748f81bdd513
jormunrod <jorgemr@protonmail.ch>
2025-04-03 11:42:21 +0200

    feat: add StringChallenge project

---
+++ Challenges/StringChallenge/Program.cs
@@ -0,0 +1,31 @@
+﻿// C# code​​​​​​‌‌​‌​​​‌​​​‌‌‌​‌​​‌‌‌‌‌‌‌ below
+using System;
+
+// Write your answer here, and then test your code.
+// Don't change the structure of the Answer class.
+public class Answer {
+
+    // Change these Boolean values to control whether you see 
+    // the expected result and/or hints.
+    public  static Boolean ShowExpectedResult = false;
+    public  static Boolean ShowHints = false;
+
+    public static string FormatDateTime(DateTime the_date) {
+        // Your code goes here
+        string year = the_date.Year.ToString();
+        string month = the_date.Month.ToString();
+        string day = the_date.Day.ToString();
+
+        string[] data = {year, month, day};
+
+        string output = String.Join("--", data);
+        
+        return output;
+    }
+
+    public static string FormatJoinStrings(string[] strs) {
+        string output = String.Join(".", strs);
+        return output.ToUpper();
+    }
+
+}
